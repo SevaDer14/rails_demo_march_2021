8cceb294c91e6baf507aab8a77b45b6657b30e37
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-20 10:35:42 +0200

    WIP making create action

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -4,9 +4,12 @@ class Api::ArticlesController < ApplicationController
     render json: { articles: articles }
   end
 
-def show
-  article = Article.find(params[:id])
-  render json: { article: article }  
-end
+  def show
+    article = Article.find(params[:id])
+    render json: { article: article }  
+  end
 
+  def create 
+    binding.pry
+  end
 end
